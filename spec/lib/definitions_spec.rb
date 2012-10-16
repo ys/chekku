e8cbf0bf97705ee150625963568d1635a5b5d33f
Yannick Schutz <yannick.schutz@gmail.com>
2012-10-16 23:15:10 +0200

    stub definitions_service call for definitions_spec

--- spec/lib/definitions_spec.rb
+++ spec/lib/definitions_spec.rb
@@ -13,11 +13,13 @@ describe Chekku::Definitions do
 
     describe '.get_definition' do
       it 'should return the definition' do
+        Chekku::DefinitionsService.any_instance.stub(:load_definitions_for)
         definitions.definitions_service.stub(:definition_for).with('mysql').and_return(definition)
         definitions.get_definition!('mysql').should == definition
       end
 
       it 'should raise an error if not found' do
+        Chekku::DefinitionsService.any_instance.stub(:load_definitions_for)
         definitions.definitions_service.stub(:definition_for).with('mysql').and_return(nil)
         expect{ definitions.get_definition!('mysql') }.to raise_error(DefinitionNotFoundError)
       end
